0323c5746e4846f874e6e21490411c65f98a8734
Andrei Rabchynski <anjykolobok@mail.ru>
2021-02-24 15:04:10 +0300

    Add day3 solutions

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,23 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double number;
+        double divider;
+
+        while (true){
+
+            number = scanner.nextDouble();
+            divider = scanner.nextDouble();
+
+            if (divider == 0) break;
+
+            System.out.println(number/divider);
+        }
 
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,21 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double number, diliver;
+
+        for (int count = 0 ; count < 5 ; count++ ){
+            number = scanner.nextDouble();
+            diliver = scanner.nextDouble();
+            if (diliver == 0) {
+                System.out.println("Деление на 0");
+                continue;
+            }
+            System.out.println(number/diliver);
+        }
 
     }
 }
